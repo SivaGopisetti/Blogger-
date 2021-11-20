5255d9e8eaa3c1527d05e1b0171ab82d50244e86
pakodi <gsknaidu1@gmail.com>
2021-11-20 20:01:57 +0530

    created components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './MyComponents/login/login.component';
+import { NavbarComponent } from './MyComponents/navbar/navbar.component';
+import { RegisterComponent } from './MyComponents/register/register.component';
+import { AddBlogComponent } from './MyComponents/add-blog/add-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegisterComponent,
+    AddBlogComponent
   ],
   imports: [
     BrowserModule,
