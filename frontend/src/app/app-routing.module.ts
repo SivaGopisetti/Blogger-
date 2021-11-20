7dcc3a0720a197ec6d81114234fe12cc4bc2ae7f
pakodi <gsknaidu1@gmail.com>
2021-11-20 23:45:58 +0530

    registation validatio

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddBlogComponent } from './MyComponents/add-blog/add-blog.component';
 import { LoginComponent } from './MyComponents/login/login.component';
 import { NavbarComponent } from './MyComponents/navbar/navbar.component';
 import { RegisterComponent } from './MyComponents/register/register.component';
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path:"register",
     component:RegisterComponent
+  },
+  {
+    path:"add-blog",
+    component:AddBlogComponent
   }
 
 ];
