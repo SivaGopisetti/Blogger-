01c638c6ceb4e3fa19b1440eef9a641fc25e4993
pakodi <gsknaidu1@gmail.com>
2021-11-20 21:31:51 +0530

    created login,register page and ui of blog

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './MyComponents/login/login.component';
+import { NavbarComponent } from './MyComponents/navbar/navbar.component';
+import { RegisterComponent } from './MyComponents/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:NavbarComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+    path:"register",
+    component:RegisterComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
