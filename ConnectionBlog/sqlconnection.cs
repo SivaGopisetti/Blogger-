17320d657f09ba04dfc08fc7ff7131481d9be5d0
sasankasrinivas99@gmail.com <sasankakakaraparti@gmail.com>
2021-11-23 18:29:34 +0530

    Add project files.

---
+++ ConnectionBlog/sqlconnection.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SqlConnectionClass
+{
+    public class SqlconnectionClass
+    {
+        public static string ConnectionString = @"server=IM-RT-LP-677\SQLEXPRESS;database=BlogApp;integrated security=True";
+    }
+}
